922c3b6df4a3eb9683b3718b7530485c447e2831
unknown <405622418@qq.com>
2017-08-20 22:18:28 +0800

    update

---
+++ Config.py
@@ -0,0 +1,15 @@
+#osu!新人群月度精彩视频集锦
+replay_bilibili = [
+    {
+        'title':'第1集2017.5',
+        'url':'https://www.bilibili.com/video/av10959103/'
+    },
+    {
+        'title':'第2集2017.6',
+        'url':'https://www.bilibili.com/video/av11800563/'
+    },
+    {
+        'title':'第3集2017.7',
+        'url':'https://www.bilibili.com/video/av12957061/'
+    }
+]
