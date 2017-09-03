62fe817b5f1bf03e94ad7e543ee51705500f8ab1
unknown <405622418@qq.com>
2017-09-03 17:22:35 +0800

    create

---
+++ plugins/Config.py
@@ -0,0 +1,19 @@
+#osu!新人群月度精彩视频集锦
+replay_bilibili = [
+    {
+        'title':'第1集2017.5',
+        'url':'https://www.bilibili.com/video/av10959103/'
+    },
+    {
+        'title':'第2集2017.6',
+        'url':'https://www.bilibili.com/video/av11800563/'
+    },
+    {
+        'title':'第3集2017.7',
+        'url':'https://www.bilibili.com/video/av12957061/'
+    },
+    {
+        'title':'第4集2017.8(第一季完结)',
+        'url':'https://www.bilibili.com/video/av14046324/'
+    }
+]
--- plugins/test.py
+++ plugins/test.py
@@ -6,8 +6,12 @@ import traceback
 import json
 import requests
 import redis
+import html.parser
 import re
 import datetime
+import mods
+
+import Config
 
 rbq_614892339 = set([])
 rbq_list_614892339 = {}            
@@ -19,7 +23,7 @@ def onQQMessage(bot, contact, member, content):
     #contact :  ctype/qq/uin/nick/mark/card/name 
     #群限制 Q号 614892339
     if contact.ctype == 'group':
-        if contact.qq != '614892339' and contact.qq != '514661057':
+        if contact.qq != '614892339' and contact.qq != '514661057' and contact.qq != '598918097':
             return 
         if len(rbq_614892339) > 20:
             rbq_614892339.pop()
@@ -28,7 +32,7 @@ def onQQMessage(bot, contact, member, content):
         if content == '!help':
             bot.SendTo(contact, 'dalou只会打爆你!')
         elif content == '!dalou':
-            msg = 'dalouBot v1.1\n1.rbq(暂时关闭)\n2.myrbq\n3.setid xxx\n4.myinfo\n5.dalou的奸视(new)\n6.check xxx(new)\n7.js 奸视列表\n8.接受功能建议'
+            msg = get_help()
             bot.SendTo(contact, msg)
         # elif content == '!rbq':
         #     r = random.choice(list(rbq_614892339))
@@ -72,6 +76,7 @@ def onQQMessage(bot, contact, member, content):
             msg = "%s\nosu:%s\nosuid:%s\nmoney:%s\nbagnum:%s\n%s" % (member.name, res[5], res[3], res[6], res[7], home_url)
             bot.SendTo(contact, msg) 
         elif '!getid' in content:
+            content = content.rstrip()
             name = content.split('@')[1]
             g = bot.List('group', str(contact.qq))[0]
             name_str = 'card=%s' % name
@@ -101,13 +106,98 @@ def onQQMessage(bot, contact, member, content):
         elif '!check' in content:
             bot.SendTo(contact, 'dalou手动计算中...请骚等!')
             uid = content[7:]
+            #取qq绑定
+            if not uid:
+                res = get_osuinfo_byqq(member.qq)
+                if not res:
+                    bot.SendTo(contact, member.name+'未绑定osuid')
+                    return
+                uid = res[5]
             pp,pp2,maxpp = check_user(uid)
             if not pp:
                 bot.SendTo(contact, '没有pp,下一个!')
                 return
-            msg = '%s\npp:%spp\n预估:%spp\n最大可能:%spp' % (uid,pp,pp2,maxpp)
+            msg = '%s\npp:%spp\ndalou手算:%spp\n目前潜力:%spp' % (uid,pp,pp2,maxpp)
             bot.SendTo(contact, msg)
-
+        elif '!test' in content:
+            uid = content[6:]
+            #取qq绑定
+            if not uid:
+                res = get_osuinfo_byqq(member.qq)
+                if not res:
+                    bot.SendTo(contact, member.name+'未绑定osuid')
+                    return
+                uid = res[5]
+            value = dalou_test(uid)
+            if value < 1:
+                level = '极致不健康'
+            elif 5 > value >= 1:
+                level = '灰常不健康'
+            elif 10 > value >= 5:
+                level = '不健康'
+            elif 15 > value >= 10:
+                level = '介于健康与不健康'
+            elif 25 > value >= 15:
+                level = '健康'
+            elif 100 > value >= 30:
+                level = '健康过度'
+            elif value >= 100:
+                level = '健康溢出'
+            msg = '%s\n健康指数:%s\nleve:%s' % (uid,value,level)
+            # msg = html.parser.unescape(msg)
+            bot.SendTo(contact, msg)
+        elif '!bbp' in content:
+            uid = content[5:]
+            #取qq绑定
+            if not uid:
+                res = get_osuinfo_byqq(member.qq)
+                if not res:
+                    bot.SendTo(contact, member.name+'未绑定osuid')
+                    return
+                uid = res[5]
+            msg = get_bp_info(uid)
+            bot.SendTo(contact, msg)
+        elif '!sp' == content:
+            msg = get_xinrenqun_replay()
+            bot.SendTo(contact, msg)
+        elif '!skill' in content:
+            uid = content[6:]
+            #取qq绑定
+            if not uid:
+                res = get_osuinfo_byqq(member.qq)
+                if not res:
+                    bot.SendTo(contact, member.name+'未绑定osuid')
+                    return
+                uid = res[5]
+            msg = get_skill(uid)
+            bot.SendTo(contact, msg)
+        elif '!vssk' in content:
+            ulist = content[6:].split(',')
+            if len(ulist) == 1:
+                #取qq绑定
+                res = get_osuinfo_byqq(member.qq)
+                if not res:
+                    bot.SendTo(contact, member.name+'未绑定osuid')
+                    return
+                u1 = res[5]
+                u2 = content[6:]
+            elif len(ulist) == 2:
+                u1 = ulist[0]
+                u2 = ulist[1]
+            else:
+                bot.SendTo(contact, '不想理你!')
+            msg = skill_vs(u1,u2)
+            bot.SendTo(contact, msg)
+        elif '!upage' in content:
+            slist = content[7:].split(',')
+            if len(slist) == 1:
+                page = 1
+            else:
+                page = int(slist[1])
+            msg = get_userpage(slist[0], page)
+            bot.SendTo(contact, msg)
+            
+            
     
 
     #测试
@@ -194,6 +284,7 @@ def get_cursor():
     return conn.cursor()
 
 def setid(qq, osuid, name, groupid, osuname):
+    '''qq绑定osuid'''
     try:
         cur = get_cursor()
         sql = '''
@@ -214,6 +305,7 @@ def setid(qq, osuid, name, groupid, osuname):
         return 0
 
 def get_osuid(osuname):
+    '''抓osuid'''
     try:
         url = 'https://osu.ppy.sh/api/get_user?k=%s&u=%s' % (osu_api_key, osuname)
         res = get_url(url)
@@ -235,6 +327,7 @@ def get_osuid(osuname):
         return 0
 
 def get_myinfo(qq):
+    '''qq绑定信息'''
     try:
         cur = get_cursor()
         sql = '''
@@ -251,6 +344,7 @@ def get_myinfo(qq):
 
 
 def get_osuinfo_byqq(qq):
+    '''查库qq对应osuid'''
     try:
         cur = get_cursor()
         sql = '''
@@ -266,6 +360,7 @@ def get_osuinfo_byqq(qq):
         return 0
 
 def get_bp_and_pp(uid):
+    '''用户pp和bp'''
     try:
         url = 'https://osu.ppy.sh/api/get_user?k=%s&u=%s' % (osu_api_key, uid)
         res = get_url(url)
@@ -302,6 +397,7 @@ def get_bp_and_pp(uid):
 
 
 def check_user(uid):
+    '''pp估计计算'''
     try:
         pp,res = get_bp_and_pp(uid)
         if not pp:
@@ -317,8 +413,8 @@ def check_user(uid):
             c300 = float(r['count300'])
             cmiss = float(r['countmiss'])
             acc = round((c50*50+c100*100+c300*300)/(c50+c100+c300+cmiss)/300*100,2)
-            acc1 = acc - 1
-            acc2 = acc + 1
+            acc1 = acc - 0.2
+            acc2 = acc + 0.2
             args = [r['beatmap_id'], r['enabled_mods'], maxcombo1, maxcombo2, acc1, acc2]
             # print(args)
             cur = get_cursor()
@@ -331,7 +427,7 @@ def check_user(uid):
             if res[0] is None:
                 continue
             # print(res)
-            if res[0] >= maxpp:
+            if res[0] > maxpp:
                 maxpp = res[0]
             if res[1] != 1: 
                 count_num += 1
@@ -340,7 +436,202 @@ def check_user(uid):
             yugu_pp = pp
         else:
             yugu_pp = round(count_pp/count_num)
+        if maxpp == 0:
+            maxpp = float(pp)
         return pp,yugu_pp,round(maxpp)
     except:
         traceback.print_exc()
         return 0,0,0
+
+def get_user_and_bp(uid):
+    '''用户以及Bp信息'''
+    try:
+        url = 'https://osu.ppy.sh/api/get_user?k=%s&u=%s' % (osu_api_key, uid)
+        res = get_url(url)
+        c = 0
+        while res == 0:
+            if c > 5:
+                return 0,0
+            else:
+                c += 1
+                res = get_url(url)
+        if res == 0:
+            return 0,0
+        result = json.loads(res.text)
+        if not result:
+            return 0,0
+        user = result[0]
+
+        url2 = 'https://osu.ppy.sh/api/get_user_best?k=%s&u=%s' % (osu_api_key, uid)
+        res = get_url(url2)
+        c = 0
+        while res == 0:
+            if c > 5:
+                return 0,0
+            else:
+                c += 1
+                res = get_url(url2)
+        if res == 0:
+            return 0,0
+        result = json.loads(res.text)
+        return user,result
+    except:
+        traceback.print_exc()
+        return 0,0
+
+def dalou_test(uid):
+    '''健康指数'''
+    user,bp = get_user_and_bp(uid)
+    if not user:
+        return 0
+    pc = int(user['playcount'])
+    pp = float(user['pp_raw'])
+    bp1 = float(bp[0]['pp'])
+    bp5 = float(bp[4]['pp'])
+    # value = round(pp/(5*bp1-4*bp5),2)
+    value = round(pc*pp/(5*bp1-4*bp5)/10000,2)
+    return value
+
+def get_bp_info(uid):
+    '''移植玩家bp5'''
+    try:
+        url = 'https://osu.ppy.sh/api/get_user_best?k=%s&u=%s' % (osu_api_key, uid)
+        res = requests.get(url, timeout=3)
+        s_msg = str(uid)+"'s bp!!\n"
+        result = json.loads(res.text)
+        if not result:
+            return '没有Bp,下一个!!'
+        for i,r in enumerate(result[0:5]):
+            msg = 'bp{x},{pp}pp,{acc}%,{rank},+{mod}'
+            c50 = float(r['count50'])
+            c100 = float(r['count100'])
+            c300 = float(r['count300'])
+            cmiss = float(r['countmiss'])
+            acc = round((c50*50+c100*100+c300*300)/(c50+c100+c300+cmiss)/300*100,2)
+            msg = msg.format(x=i+1,pp=round(float(r['pp'])),acc=acc,rank=r['rank'],mod=','.join(mods.getMod(int(r['enabled_mods']))))
+            s_msg = s_msg + msg + '\n'
+        return s_msg[0:-1]
+    except:
+        traceback.print_exc()
+        print('获取%s的bp失败'% str(uid))
+        return '没有Bp,下一个!!'
+
+def get_xinrenqun_replay():
+    '''新人群视频'''
+    try:
+        ship = Config.replay_bilibili
+        s_msg = 'osu!新人群月度精彩视频集锦\n'
+        for sp in ship:
+            s_msg = s_msg + sp['title'] + ' : ' + sp['url'] + '\n'
+        return s_msg[0:-1]
+    except:
+        traceback.print_exc()
+
+def get_skill(uid):
+    '''skill'''
+    try:
+        res = requests.get('http://osuskills.tk/user/'+str(uid),timeout=5)
+        if not res:
+            return '没有数据,太弱了!!'
+        s_msg = uid+"'s skill\n"
+        value = re.compile(r'<output class="skillValue">(.*?)</output>')
+        values = value.findall(res.text)
+        if not values:
+            return '那个破网站连不上!!'
+        skills = ['Stamina', 'Tenacity', 'Agility', 'Accuracy', 'Precision', 'Reaction', 'Memory', 'Reading']
+        #skills_list = list(map(lambda x,y:x+y ,skills,values))
+        for i,s in enumerate(skills):
+            val = int(values[i])
+            if  1000 > val >= 100:
+                snum = int(values[i][0:1])
+            elif val >= 1000:
+                snum = int(values[i][0:2])
+            else:
+                snum = 0
+            star = '*' * snum
+            skillkey = '%s:' % s
+            valueskey = '%s ' % values[i]
+            s_msg = s_msg+skillkey+valueskey+star+'\n'
+        return s_msg[0:-1]
+    except:
+        traceback.print_exc()
+        return '那个破网站连不上!!'
+
+def skill_vs(uid,uid2):
+    try:
+        res = requests.get('http://osuskills.tk/user/%s/vs/%s'%(uid,uid2),timeout=5)
+        if not res:
+            return '实力太强p坏了,你们还是去床上解决吧!!'
+        value = re.compile(r'<output class="skillValue">(.*?)</output>')
+        values = value.findall(res.text)
+        if not values:
+            return '那个破网站连不上,你们还是去床上解决吧!!'
+        skills = ['Stamina', 'Tenacity', 'Agility', 'Accuracy', 'Precision', 'Reaction', 'Memory', 'Reading']
+        s_msg = '%s vs %s\n'%(uid,uid2)
+        for i,s in enumerate(skills):
+            v1 = int(values[i])
+            v2 = int(values[i+8])
+            vv = str(abs(v1-v2))
+            fuhao = ' -- '
+            if v1 > v2:
+                s_msg = s_msg + s + ' : ' + values[i]+'(+'+vv+')' + fuhao + values[i+8] +'\n'
+            elif v1 < v2:
+                s_msg = s_msg + s + ' : ' + values[i] + fuhao + values[i+8] +'(+'+vv+')'+'\n'
+            else:
+                s_msg = s_msg + s + ' : ' + values[i] + fuhao + values[i+8] +'\n'
+        return s_msg[0:-1]
+    except:
+        traceback.print_exc()
+        return '那个破网站连不上,你们还是去床上解决吧!!'
+
+def get_userpage(uid, page):
+    try:
+        if page <= 0:
+            return '你是想找Bug吗??'
+        res = requests.get('https://osu.ppy.sh/api/get_user?k=b68fc239f6b8bdcbb766320bf4579696c270b349&u='+str(uid),timeout=5)
+        if not res:
+            return 'id都错了醒醒!'
+        s_msg = uid+"'s userpage   "
+        result = json.loads(res.text)
+        if not result:
+            return 'id都错了醒醒!'
+        uid = result[0]['user_id']
+
+        url = 'https://osu.ppy.sh/pages/include/profile-userpage.php?u=%s'
+        res = requests.get(url % uid,timeout=5)
+        if len(res.text) < 1:
+            return 'support都没有,先氪金好吧!'
+        result = (res.text).replace('<br />','\n')
+        repatt = re.compile(r'<.*?>')
+        result = re.sub(repatt,'',result)
+        result = html.parser.unescape(result)
+        pagesize = 250
+        total = (len(result)+pagesize)//pagesize
+        if page > total:
+            page = total
+        s_msg = s_msg + '第%s页,共%s页\n'%(str(page),str(total))
+        return s_msg + result[pagesize*(page-1):pagesize*page]
+    except:
+        traceback.print_exc()
+        return 'ppy炸了,请骚等!!'
+
+
+def get_help():
+    '''帮助'''
+    msg = '''dalouBot v1.2
+1.rbq(暂时关闭)
+2.myrbq
+3.setid xxx(请绑定,后续有用)
+4.myinfo
+5.getid@别人(获取群员osuid)
+6.dalou的奸视(new)(需要权限)
+7.check xxx(new)(使用大数据支持)
+8.js 奸视列表(new)
+9.test 健康指数(new)(dalou公式)
+10.bbp(移植)
+11.sp(移植)
+12.skill(移植)
+13.vssk(移植)
+14.upage xx,2(移植)
+15.接受功能建议'''
+    return msg
